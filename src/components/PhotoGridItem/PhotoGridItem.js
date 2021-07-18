1d83fbede7211cab4bb9d16eacd3aae969eeff69
Tim Austin <tim@neenjaw.com>
2021-07-18 16:59:50 -0600

    exercises with solution

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            type="image/avif"
+            srcSet={`
+              ${src.replace('.jpg', '.avif')} 1x,
+              ${src.replace('.jpg', '@2x.avif')} 2x,
+              ${src.replace('.jpg', '@3x.avif')} 3x
+            `}
+          />
+          <source
+            type="image/jpeg"
+            srcSet={`
+              ${src} 1x,
+              ${src.replace('.jpg', '@2x.jpg')} 2x,
+              ${src.replace('.jpg', '@3x.jpg')} 3x
+            `}
+          />
+          <Image alt={alt} src={src} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -13,14 +31,14 @@ const PhotoGridItem = ({ id, src, alt, tags }) => {
         ))}
       </Tags>
     </article>
-  );
-};
+  )
+}
 
 const Anchor = styled.a`
   text-decoration: none;
   color: inherit;
   outline-offset: 4px;
-`;
+`
 
 const Image = styled.img`
   display: block;
@@ -28,20 +46,25 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
-`;
+`
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
-`;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding: 4px 0;
+`
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
-`;
+  &:not(:last-of-type) {
+    margin-right: 8px;
+  }
+`
 
-export default PhotoGridItem;
+export default PhotoGridItem
